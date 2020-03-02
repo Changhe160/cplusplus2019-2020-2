dbf1800a72dda2fa89fc3247c69b31d1fc9adf4b
Changhe Li <changhe.lw@gmail.com>
2020-03-02 11:39:18 +0800

    第三章更新

--- code/chapter3.2/chapter3.2.cpp
+++ code/chapter3.2/chapter3.2.cpp
@@ -5,9 +5,56 @@ using namespace std;
 
 int main() {
 
+	//例3.3 求解一元二次方程的根
+	{
+		double a, b, c;
+		cout << "请输入a(a!=0),b,c:";
+		cin >> a >> b >> c;
+		if (a != 0) { //方程
+			double delta = b * b - 4 * a*c;
+			if (delta > 0) {
+				double x1, x2;
+				delta = sqrt(delta);
+				x1 = (-b + delta) / (2 * a);
+				x2 = (-b - delta) / (2 * a);
+				cout << "方程有两个实根,分别为：" << x1 << ", " << x2 << endl;
+			}
+			else if (delta < 0) {
+				cout << "方程无实根" << endl;
+			}
+			else {
+				cout << "方程有两个相同的实根：" << -b / (2 * a) << endl;
+			}
+		}
+		else {
+			cout << "a不能为0" << endl;
+		}
+
+	}
+
 	{//3.2.1节
 		if (int i = 10) {
 			/*...*/
+		}
+	}
+
+	{//例3.2
+		unsigned score;
+		cout << "请输入一个分数:";
+		cin >> score;
+		if (score < 60) {
+			cout << "F" << endl;
+		}
+		else if (score < 70) {
+			cout << "D" << endl;
+		}
+		else if (score < 80) {
+			cout << "C" << endl;
+		}else if (score < 90) {
+			cout << "B" << endl;
+		}
+		else {
+			cout << "A" << endl;
 		}
 	}
 
@@ -86,30 +133,5 @@ int main() {
 		}
 	}
 
-	//例3.3 求解一元二次方程的根
-	{
-		double a, b, c;
-		cout << "请输入a(a!=0),b,c:";
-		cin >> a >> b >> c;
-		if (a != 0) { //方程
-			double delta = b * b - 4 * a*c;
-			if (delta > 0) {
-				double x1, x2;
-				delta = sqrt(delta);
-				x1 = (-b + delta) / (2 * a);
-				x2 = (-b - delta) / (2 * a);
-				cout << "方程有两个实根,分别为：" << x1 << ", " << x2 << endl;
-			}
-			else if (delta < 0) {
-				cout << "方程无实根" << endl;
-			}
-			else {
-				cout << "方程有两个相同的实根：" << -b / (2 * a) << endl;
-			}
-		}
-		else {
-			cout << "a不能为0" << endl;
-		}
-
-	}
+	
 }
